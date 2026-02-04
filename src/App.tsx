1e5e0f79ad40930dd2fabd61e9bff8f7f36b3d2c
Γιαννης `Κοζυρης <kozyris@macgiannis-2.local>
2026-02-04 07:27:39 +0200

    add smooth scrolling with lenis and fix contrast and mobile layout issues

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,22 @@ import JournalPostPage from './pages/JournalPostPage';
 import { ContactProvider } from './context/ContactContext';
 import ContactModal from './components/ContactModal';
 
+import SmoothScroll from './components/SmoothScroll';
+
 function App() {
   return (
     <ContactProvider>
-      <Router>
-        <ContactModal />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/journal" element={<JournalPage />} />
-          <Route path="/journal/:postId" element={<JournalPostPage />} />
-        </Routes>
-      </Router>
+      <SmoothScroll>
+        <Router>
+          <ContactModal />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/journal" element={<JournalPage />} />
+            <Route path="/journal/:postId" element={<JournalPostPage />} />
+          </Routes>
+        </Router>
+      </SmoothScroll>
     </ContactProvider>
   );
 }
---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,37 @@
+import { useLayoutEffect } from 'react';
+import Lenis from 'lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+    useLayoutEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.5,
+            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            orientation: 'vertical',
+            gestureOrientation: 'vertical',
+            smoothWheel: true,
+            wheelMultiplier: 1,
+            touchMultiplier: 2,
+        });
+
+        lenis.on('scroll', ScrollTrigger.update);
+
+        const update = (time: number) => {
+            lenis.raf(time * 1000);
+        };
+
+        gsap.ticker.add(update);
+
+        gsap.ticker.lagSmoothing(0);
+
+        return () => {
+            lenis.destroy();
+            gsap.ticker.remove(update);
+        };
+    }, []);
+
+    return <>{children}</>;
+}
